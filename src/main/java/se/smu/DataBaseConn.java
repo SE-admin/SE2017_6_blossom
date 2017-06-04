f27e253967811058b528c8b63000942b1a8f8f78
201511054 <201511054@sangmyung.kr>
2017-06-04 21:18:22 +0900

    임현			1.1.0.		2017-06-04	MYSQL 다른 버전용 명령어 추가

--- src/main/java/se/smu/DataBaseConn.java
+++ src/main/java/se/smu/DataBaseConn.java
@@ -1,7 +1,7 @@
 /**
  * @title : DataBaseConn.java
  * @author : 임현 (201511054@sangmyung.kr)
- * @version : 1.0.2.
+ * @version : 1.1.0.
  * @since : 2017 - 06 - 04
  * @brief : 데이터 베이스 연동 String
  * ------------------------------
@@ -10,23 +10,20 @@
  	임현			1.0.0.		2017-06-04	초안 작성
  	임현			1.0.1.		2017-06-04	주석 오타 수정 및 주석 추가
  	임현			1.0.2.		2017-06-04	아이디, 비밀번호 추가
+ 	임현			1.1.0.		2017-06-04	MYSQL 다른 버전용 명령어 추가
  * ------------------------------
  */
 
 package se.smu;
 
 public class DataBaseConn {
-	// Class.forName(DataBaseConn.forName);
 	public static String forName = "com.mysql.cj.jdbc.Driver";
+	// public static String forName = "com.mysql.jdbc.Driver";
 	
-	// conn = DriverManager.getConnection(DataBaseConn.getConn, "root", "0000");
-	
-	// URL 스트링
 	public static String URL = "jdbc:mysql://localhost:3306/mysql?serverTimezone=UTC&useSSL=false";
+	// public static String URL = "jdbc:mysql://localhost:3306/mysql?=UTC&useSSL=false";
 	
-	// 아이디 스트링
 	public static String ID = "root";
 	
-	// 비밀번호 스트링
 	public static String PASSWORD = "0000";
 }
