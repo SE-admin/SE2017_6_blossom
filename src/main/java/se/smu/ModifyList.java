b2af92f5fa93079fde509314cb3307223505ea60
201511054 <201511054@sangmyung.kr>
2017-06-04 23:37:32 +0900

    황은선			1.2.3.		2017-06-04	DB에 CourName,Hide행 추가

--- src/main/java/se/smu/ModifyList.java
+++ src/main/java/se/smu/ModifyList.java
@@ -1,7 +1,7 @@
 /**
  * @title : ModifyList.java
  * @author : 황은선 (201511077@sangmyung.kr)
- * @version : 1.2.2.
+ * @version : 1.2.3.
  * @since : 2017 - 05 - 31
  * @brief : 리스트 수정 코드
  * ------------------------------
@@ -13,6 +13,7 @@
         임현			1.2.0.		2017-06-02	매개변수 추가
         임현			1.2.1.		2017-06-04	데이터베이스 연동 수정
         임현			1.2.2.		2017-06-04	데이터베이스 연동 수정
+        황은선			1.2.3.		2017-06-04	DB에 CourName,Hide행 추가
  * ------------------------------
  */
 
@@ -27,24 +28,31 @@ import java.sql.*;
 
 public class ModifyList {
 	// 기본 변수 선언
+	static String ModiCourName;
 	static String ModiListName;
 	static String ModiDeadLine;
 	static String ModiFinishDay;
 	static String ModiFinish;
 	static String ModiImportance;
+	static String ModiHide;
+	static String CourName;
 	static String ListName;
 	static String DeadLine;
 	static String FinishDay;
 	static String Finish;
 	static String Importance;
+	static String Hide;
 
 	// 수정된 List정보 입력
-	public ModifyList(String ModiListName, String ModiDeadLine, String ModiFinishDay, String ModiFinish, String ModiImportance) {
+	public ModifyList(String ModiCourName, String ModiListName, String ModiDeadLine, String ModiFinishDay, String ModiFinish, String ModiImportance, String ModiHide) {
+
+		this.ModiCourName = ModiCourName;
 		this.ModiListName = ModiListName;
 		this.ModiDeadLine = ModiDeadLine;
 		this.ModiFinishDay = ModiFinishDay;
 		this.ModiFinish = ModiFinish;
 		this.ModiImportance = ModiImportance;
+		this.ModiHide = ModiHide;
 		
 		try {
 			// 기본 변수 선언
@@ -64,17 +72,20 @@ public class ModifyList {
 			st.execute(sql);
 			rs = st.executeQuery("Select * from Listinfo");
 
-			// 입력받은 ListName과 ListDB의 정보중 일치하는 것이 있으면 삭제
+			
 			while (rs.next()) {
 				ModiListName = rs.getString("ListName");
+				CourName = rs.getString("CourName");
 				DeadLine = rs.getString("DeadLine");
 				FinishDay = rs.getString("FinishDay");
 				Finish = rs.getString("Finish");
 				Importance = rs.getString("Importance");
+				Hide = rs.getString("Hide");
 
-				if ((ModiListName.equals(ListName)) || (ModiDeadLine.equals(DeadLine))
-						|| (ModiFinishDay.equals(FinishDay)) || (ModiFinish.equals(Finish))
-						|| (ModiImportance.equals(Importance))) {
+				if ((ModiCourName.equals(CourName)) || (ModiListName.equals(ListName)))
+				{
+					sql = "UPDATE Listinfo SET CourName=" + "\'"+ModiCourName+"\'" + "where CourName=" + "\'" +CourName+ "\'";
+					st.execute(sql);
 					sql = "UPDATE Listinfo SET ListName=" + "\'"+ModiListName+"\'" + "where ListName=" + "\'" +ListName+ "\'";
 					st.execute(sql);
 					sql = "UPDATE Listinfo SET DeadLine=" + "\'"+ModiDeadLine+"\'" + "where DeadLine=" + "\'" +DeadLine+ "\'";
@@ -84,6 +95,8 @@ public class ModifyList {
 					sql = "UPDATE Listinfo SET Finish=" + "\'"+ModiFinish+"\'" + "where Finish=" + "\'" +Finish+ "\'";
 					st.execute(sql);
 					sql = "UPDATE Listinfo SET Importance=" + "\'"+ModiImportance+"\'" + "where Importance=" + "\'" +Importance+ "\'";
+					st.execute(sql);
+					sql = "UPDATE Listinfo SET Hide=" + "\'"+ModiHide+"\'" + "where Hide=" + "\'" +Hide+ "\'";
 					st.execute(sql);
 				}
 			}
@@ -98,6 +111,6 @@ public class ModifyList {
 	}
 
 	public static void main(String[] args) {
-		new ModifyList(ModiListName, ModiDeadLine, ModiFinishDay, ModiFinish, ModiImportance);
+		new ModifyList(ModiCourName, ModiListName, ModiDeadLine, ModiFinishDay, ModiFinish, ModiImportance, ModiHide);
 	}
 }
