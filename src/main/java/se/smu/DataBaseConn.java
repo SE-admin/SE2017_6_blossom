dcd0095a8e366a055488124c10bbfa84066c89ba
201511054 <201511054@sangmyung.kr>
2017-06-05 22:05:02 +0900

    임현			1.2.1.		2017-06-05	localhost 복구

--- src/main/java/se/smu/DataBaseConn.java
+++ src/main/java/se/smu/DataBaseConn.java
@@ -1,7 +1,7 @@
 /**
  * @title : DataBaseConn.java
  * @author : 임현 (201511054@sangmyung.kr)
- * @version : 1.2.0.
+ * @version : 1.2.1.
  * @since : 2017 - 06 - 04
  * @brief : 데이터 베이스 연동 String
  * ------------------------------
@@ -12,19 +12,29 @@
  	임현			1.0.2.		2017-06-04	아이디, 비밀번호 추가
  	임현			1.1.0.		2017-06-04	MYSQL 다른 버전용 명령어 추가
  	임현			1.2.0.		2017-06-05	외부 MYSQL 서버 구축 자하랑 PC (H409)
+ 	임현			1.2.1.		2017-06-05	localhost 복구
  * ------------------------------
  */
 
 package se.smu;
 
 public class DataBaseConn {
-	// public static String forName = "com.mysql.cj.jdbc.Driver";
-	public static String forName = "com.mysql.jdbc.Driver";
+	public static String forName = "com.mysql.cj.jdbc.Driver";
+	// public static String forName = "com.mysql.jdbc.Driver";
 	
+	// 자신의 컴퓨터로 접속
+	public static String URL = "jdbc:mysql://localhost:3306/mysql?serverTimezone=UTC&useSSL=false";
+	// public static String URL = "jdbc:mysql://localhost:3306/mysql?=UTC&useSSL=false";
+	
+	// 외부 서버 연결
 	// public static String URL = "jdbc:mysql://192.168.0.35:3306/mysql?serverTimezone=UTC&useSSL=false";
-	public static String URL = "jdbc:mysql://192.168.0.35:3306/mysql?=UTC&useSSL=false";
+	// public static String URL = "jdbc:mysql://192.168.0.35:3306/mysql?=UTC&useSSL=false";
 	
+	//public static String URL = "jdbc:sqlplus://mango.smu.ac.kr:3306/mysql?serverTimezone=UTC&useSSL=false";
+	
+	// 아이디
 	public static String ID = "root";
 	
+	// 비밀번호
 	public static String PASSWORD = "0000";
 }
